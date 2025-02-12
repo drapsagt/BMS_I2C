d533e26a14784c6f1e1e58e92dcd5cd3b2451ca1
drapsagt <gaspard.torris@gmail.com>
2025-02-12 23:10:29 +0100

    feat :  add the request for the voltage of a module

--- master_module/src/I2S_module.cpp
+++ master_module/src/I2S_module.cpp
@@ -14,6 +14,18 @@ I2S_module::~I2S_module()
 
 void I2S_module::askVoltage()
 {
+    int voltage = 0;
+    wire->beginTransmission(this->_address);
+    wire->write(0x01); // Command to ask for voltage
+    wire->endTransmission();
+    wire->requestFrom(this->_address, 2); // Request 2 bytes from the device
+
+    if (wire->available() == 2)
+    {
+        voltage = wire->read() << 8 | wire->read();
+    }
+
+    this->_voltage = voltage;
 }
 
 void I2S_module::askTemperature()
